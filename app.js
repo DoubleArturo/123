c014abb4105aaefa0ea256279968b17255ba5167
doublearturo <ch47992001@gmail.com>
2021-10-19 20:53:53 +0800

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 const PORT = 3000
+const db = mongoose.connection
+
+//設定資料庫連線
+mongoose.connect('mongodb://localhost/shortURL')
+
+//取得db連線狀態
+db.on('errer', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
 
 //設定路由
 app.get('/', (req, res) => {
